9d65e04109cce996b1caa5098729b65a606d6486
ArpitShukla3 <arpit900shukla@gmail.com>
2024-11-09 15:17:27 +0530

    fixing

--- utils.js
+++ utils.js
@@ -4,6 +4,6 @@ export const createCookieOptions = () => {
     maxAge: 5 * 24 * 60 * 60 * 1000, // Note: fixed the 100 multiplier to 1000 to represent milliseconds
     httpOnly: false,
     secure: true,
-    sameSite: "None",
+    sameSite: "Lax",
   };
 };
