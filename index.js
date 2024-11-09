21b1c79bd852cb26b1b69320631b92b492118134
ArpitShukla3 <arpit900shukla@gmail.com>
2024-11-09 14:55:09 +0530

    fixing

--- index.js
+++ index.js
@@ -18,14 +18,18 @@ const port = process.env.PORT;
 const server = http.createServer(app);
 
 // Configure CORS
-const corsOptions = {
-  origin: "http://localhost:5173",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
+// const corsOptions = {
+//   origin: "http://localhost:5173",
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   credentials: true,
+//   optionsSuccessStatus: 204,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors({
+  origin: [ 'http://localhost:5173', 'https://ruralsync-service-provider.vercel.app'],
+  credentials: true
+}))
 app.use(express.json());
 app.use(cookieParser());
 
